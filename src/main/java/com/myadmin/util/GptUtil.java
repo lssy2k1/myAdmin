7ded9f93aacc16c6ec5bbfcefb8a99417542da91
helloyeojin <bonuibomb@gmail.com>
2023-05-15 13:14:15 +0900

    student 추가

--- src/main/java/com/myadmin/util/GptUtil.java
+++ src/main/java/com/myadmin/util/GptUtil.java
@@ -13,7 +13,7 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
 public class GptUtil {
-    private static final String API_KEY = "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";
+    private static final String API_KEY = "";
     private static final String API_ENDPOINT = "https://api.openai.com/v1/chat/completions";
 
     public static String getResult(String msg) throws URISyntaxException, IOException, InterruptedException, ParseException {
