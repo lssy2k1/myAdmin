0f23afc2c5abfaf58a13f47c79d9f615762ee3c6
lssy2k1 <lssy2k1@gmail.com>
2023-06-27 20:49:06 +0900

    6/27 co-work with = first ncp upload version. redisconfig fix. pom.xml fix

--- src/main/java/com/myadmin/RedisConfig.java
+++ src/main/java/com/myadmin/RedisConfig.java
@@ -1,18 +1,42 @@
 package com.myadmin;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisPassword;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
 
 @Configuration
 public class RedisConfig {
+
+    @Value("${spring.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.redis.port}")
+    private int redisPort;
+
+//    @Value("${spring.redis.password}")
+//    private String redisPwd;
+
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(); // Redis 연결을 위한 LettuceConnectionFactory 생성
+        return new LettuceConnectionFactory(redisHost, redisPort); // Redis 연결을 위한 LettuceConnectionFactory 생성
     }
+
+//    @Bean
+//    public RedisConnectionFactory redisConnectionFactory() {
+//        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+//        redisStandaloneConfiguration.setHostName(redisHost);
+//        redisStandaloneConfiguration.setPort(redisPort);
+////        redisStandaloneConfiguration.setPassword(redisPwd);
+//        LettuceConnectionFactory lettuceConnectionFactory = new LettuceConnectionFactory(redisStandaloneConfiguration);
+//        return lettuceConnectionFactory;
+//    }
+
 
     @Bean
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
