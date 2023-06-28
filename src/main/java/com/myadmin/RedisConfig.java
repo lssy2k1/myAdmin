8153b876e65c3ec311f982307d6ede8b903b1fbc
lssy2k1 <lssy2k1@gmail.com>
2023-06-28 22:29:56 +0900

    6/27 fix calendar -> add redis pwd

--- src/main/java/com/myadmin/RedisConfig.java
+++ src/main/java/com/myadmin/RedisConfig.java
@@ -19,23 +19,23 @@ public class RedisConfig {
     @Value("${spring.redis.port}")
     private int redisPort;
 
-//    @Value("${spring.redis.password}")
-//    private String redisPwd;
-
-    @Bean
-    public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(redisHost, redisPort); // Redis 연결을 위한 LettuceConnectionFactory 생성
-    }
+    @Value("${spring.redis.password}")
+    private String redisPwd;
 
 //    @Bean
 //    public RedisConnectionFactory redisConnectionFactory() {
-//        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
-//        redisStandaloneConfiguration.setHostName(redisHost);
-//        redisStandaloneConfiguration.setPort(redisPort);
-////        redisStandaloneConfiguration.setPassword(redisPwd);
-//        LettuceConnectionFactory lettuceConnectionFactory = new LettuceConnectionFactory(redisStandaloneConfiguration);
-//        return lettuceConnectionFactory;
+//        return new LettuceConnectionFactory(redisHost, redisPort); // Redis 연결을 위한 LettuceConnectionFactory 생성
 //    }
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+        redisStandaloneConfiguration.setHostName(redisHost);
+        redisStandaloneConfiguration.setPort(redisPort);
+        redisStandaloneConfiguration.setPassword(redisPwd);
+        LettuceConnectionFactory lettuceConnectionFactory = new LettuceConnectionFactory(redisStandaloneConfiguration);
+        return lettuceConnectionFactory;
+    }
 
 
     @Bean
